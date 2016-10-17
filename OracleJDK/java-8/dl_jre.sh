477e46a53d0ca4ab6e0fc9ba817a586c342b0d68
kzheng <kevin.a.zheng@acenture.com>
2016-10-17 16:17:37 +1100

    changing to DL

---
+++ OracleJDK/java-8/dl_jre.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+ggID='0B-9eqV_NWd3oTmdNQklZMXBUSk0'
+ggURL='https://drive.google.com/uc?export=download'
+filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
+getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
+curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "${filename}"
