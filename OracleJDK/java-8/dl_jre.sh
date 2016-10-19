05c527790b19381c14cd390b568cb40d9ffd7147
cybrkit <cybrkit@users.noreply.github.com>
2016-10-19 15:29:50 +1100

    Update dl_jre.sh

--- OracleJDK/java-8/dl_jre.sh
+++ OracleJDK/java-8/dl_jre.sh
@@ -1,7 +1,2 @@
 #!/usr/bin/env bash
-
-ggID='0B-9eqV_NWd3oTmdNQklZMXBUSk0'
-ggURL='https://drive.google.com/uc?export=download'
-filename="$(curl -sc /tmp/gcokie "${ggURL}&id=${ggID}" | grep -o '="uc-name.*</span>' | sed 's/.*">//;s/<.a> .*//')"
-getcode="$(awk '/_warning_/ {print $NF}' /tmp/gcokie)"
-curl -Lb /tmp/gcokie "${ggURL}&confirm=${getcode}&id=${ggID}" -o "/tmp/${filename}"
+curl -L "https://www.dropbox.com/s/ggfpy9hs3ybrkw1/server-jre-8u101-linux-x64.tar.gz?dl=1" -o "/tmp/server-jre-8u101-linux-x64.tar.gz"
